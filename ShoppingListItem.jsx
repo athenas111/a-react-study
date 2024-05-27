61be9c8e0347cd074125f6dbdcef0798eeb9409b
athenas111 <146963873+athenas111@users.noreply.github.com>
2024-05-27 15:13:52 -0300

    Create ShoppingListItem.jsx

---
+++ ShoppingListItem.jsx
@@ -0,0 +1,8 @@
+export default function ShopListItem({item, quantity, completed}) {
+    const styles = {color:  completed? 'grey': 'red',
+     textDecoration: completed ? 'line-through':'none'}
+   return <li style={styles}>
+    {item} - {quantity}
+   </li>
+}
+{/* <li key={i.id} style={{color: i.completed? 'grey': 'red', textDecoration: i.completed ? 'line-through':'none',}}>{i.item} - {i.quantity}</li>  */}
